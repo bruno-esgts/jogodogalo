3a7d3875519ecb6b5e6e5207c930a99018b9f258
Bruno Jesus <bruno.jesus@esg.ipsantarem.pt>
2024-04-24 14:35:58 +0100

    Add JavaDoc comments

--- src/main/java/pt/brunojesus/jogodogalo/Board.java
+++ src/main/java/pt/brunojesus/jogodogalo/Board.java
@@ -16,15 +16,52 @@ import pt.brunojesus.jogodogalo.validator.play.GameAlreadyFinishedPlayValidator;
 import pt.brunojesus.jogodogalo.validator.play.PlayValidator;
 import pt.brunojesus.jogodogalo.validator.play.PositionInUseValidator;
 
+/**
+ * The Class Board.
+ * <br/>
+ * Represents a Tic-Tac-Toe board.
+ * 
+ * @author Bruno Jesus
+ * @version 1.0
+ * @since 1.0
+ */
 public class Board {
 
+	/** The board. */
 	private BoardItemEnum[][] board;
+	
+	/** The size. */
 	private Integer size;
+	
+	/** The last played item. */
 	private BoardItemEnum lastPlayedItem = null;
+	
+	/** The winner. */
 	private BoardItemEnum winner = null;
+	
+	/** The winner check commands. */
 	private List<CheckWinnerCommand> winnerCheckCommands = null;
+	
+	/** The play validators. */
 	private List<PlayValidator> playValidators = null;
 
+	
+	/**
+	 * Instantiates a new board with the default
+	 * {@link CheckWinnerCommand}s and {@link PlayValidator}s.
+	 *
+	 * @param size the size of the board on width x height
+	 * @see CheckWinnerCommand
+	 * @see PlayValidator
+	 * @see CheckLineWinnerCommand
+	 * @see CheckColumnWinnerCommand
+	 * @see CheckDiagonalWinnerCommand
+	 * @see CheckInverseDiagonalWinnerCommand
+	 * @see GameAlreadyFinishedPlayValidator
+	 * @see BoardPositionValidator
+	 * @see ConsecutivePlayValidator
+	 * @see PositionInUseValidator
+	 */
 	public Board(int size) {
 		this(
 				size,
@@ -43,6 +80,14 @@ public class Board {
 		);
 	}
 	
+	/**
+	 * Instantiates a new board with the provided {@link java.util.List} of
+	 * {@link CheckWinnerCommand} and {@link PlayValidator}
+	 *
+	 * @param size the size
+	 * @param winnerCheckCommands the winner check commands
+	 * @param playValidators the play validators
+	 */
 	public Board(
 			int size, 
 			List<CheckWinnerCommand> winnerCheckCommands,
@@ -54,6 +99,16 @@ public class Board {
 		this.playValidators = playValidators;
 	}
 
+	/**
+	 * Play on the desired position.
+	 * 
+	 * If the last played {@link BoardItemEnum} is <b>CROSS</b> then 
+	 * a circle will be played, and vice versa.
+	 * 
+	 * @param x the x coordinate to play
+	 * @param y the y coordinate to play
+	 * @throws IllegalPlayException if an invalid play occurs.
+	 */
 	public void play(int x, int y) throws IllegalPlayException {
 		final BoardItemEnum item = this.lastPlayedItem == BoardItemEnum.CROSS ? 
 				BoardItemEnum.CIRCLE : BoardItemEnum.CROSS;
@@ -61,6 +116,15 @@ public class Board {
 		play(x, y, item);
 	}
 
+	/**
+	 * Play on the desired position.
+	 *
+	 * @param x the x coordinate to play
+	 * @param y the y coordinate to play
+	 * @param item the item to be played
+	 * @return the winner if exists, otherwise returns <b>null</b>
+	 * @throws IllegalPlayException if an invalid play occurs.
+	 */
 	public BoardItemEnum play(int x, int y, BoardItemEnum item) throws IllegalPlayException {
 		Optional<IllegalPlayException> exception = this.playValidators.stream()
 		.map((validator) -> validator.validatePlay(x, y, item, this))
@@ -95,22 +159,48 @@ public class Board {
 		return this.winner;
 	}
 	
+	
+	/**
+	 * Gets the item in position.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @return the item in position
+	 */
 	public BoardItemEnum getItemInPosition(int x, int y) {
 		return this.board[y][x];
 	}
 	
+	/**
+	 * Gets the winner.
+	 *
+	 * @return the winner
+	 */
 	public BoardItemEnum getWinner() {
 		return winner;
 	}
 	
+	/**
+	 * Gets the last played item.
+	 *
+	 * @return the last played item
+	 */
 	public BoardItemEnum getLastPlayedItem() {
 		return lastPlayedItem;
 	}
 	
+	/**
+	 * Gets the size.
+	 *
+	 * @return the size
+	 */
 	public int getSize() {
 		return size;
 	}
 
+	/**
+	 * Prints the.
+	 */
 	public void print() {
 		for (int y = 0; y < this.size; y++) {
 			for (int x = 0; x < this.size; x++) {
